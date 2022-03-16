d860647f340e306e29ffea9045bd661fd1f40363
Miroslav Ondra <ondra@faster.cz>
2022-03-16 17:08:28 +0100

    Add libunipidata.so

--- Makefile
+++ Makefile
@@ -5,29 +5,44 @@ LINUX_DIR_PATH = /lib/modules/$(shell uname -r)/build
 INSTALL = install
 PWD = $(shell pwd)
 WORK = overlays
-DTS_DEST_DIR = /opt/unipi/os-configurator/data/overlays
-
+DTS_DEST_DIR = /opt/unipi/os-configurator/overlays
+LIB_DEST_DIR = /opt/unipi/os-configurator
+DESCRIPTION = description.yaml
 
 #DTC_FLAGS_unipi-iris-unispi-slot12 := -@
 templates =  $(wildcard *.template)
 dtsi = $(wildcard *.dtsi)
 
 #all: $(dtsi) $(templates) $(WORK)/imx8mm-pinfunc.h
-all: $(WORK)/imx8mm-pinfunc.h
-	@python3 render-slot.py description.yaml -t template -o $(WORK)
+
+all: $(WORK)/imx8mm-pinfunc.h libunipidata.so
 	@#cp *.dtsi $(WORK)
 	MAKEFLAGS="$(MAKEFLAGS)" $(MAKE) -C $(LINUX_DIR_PATH) M=$(PWD)/$(WORK)
-
-install: $(wildcard $(WORK)/*.dtb)
-	mkdir -p $(DESTDIR)/$(DTS_DEST_DIR)
-	$(INSTALL) -m 644 $^ $(DESTDIR)/$(DTS_DEST_DIR)
 
 $(WORK)/imx8mm-pinfunc.h:
 	@mkdir -p $(WORK)
 	@ln -s $(LINUX_DIR_PATH)/arch/arm64/boot/dts/freescale/imx8mm-pinfunc.h $@
+
+$(WORK)/Makefile: $(templates) $(DESCRIPTION)
+	@python3 render-slot.py $(DESCRIPTION) -t template -o $(WORK)
+
+unipi-values.c: template/unipi-values.template.c $(DESCRIPTION)
+	@python3 render-slot.py $(DESCRIPTION) -t template -o $(WORK)
+
+unipi-values.o: unipi-values.c
+	gcc $^ -c -I unipi-hardware-id/include/ -fPIC
+
+libunipidata.so: unipi-values.o
+	gcc $^ -shared -o $@
+
+install: $(wildcard $(WORK)/*.dtb)
+	mkdir -p $(DESTDIR)/$(DTS_DEST_DIR)
+	$(INSTALL) -m 644 $^ $(DESTDIR)/$(DTS_DEST_DIR)
+	$(INSTALL) -m 644 libunipidata.so $(DESTDIR)/$(LIB_DEST_DIR)
 
 clean:
 	@touch $(WORK)/Makefile && MAKEFLAGS="$(MAKEFLAGS)" $(MAKE) -C $(LINUX_DIR_PATH) M=$(PWD)/$(WORK) clean
 	@rm -f $(WORK)/*.dts
 	@rm -f $(WORK)/Makefile
 	@rm -f $(WORK)/imx8mm-pinfunc.h
+	@rm -f libunipidata.so unipi-values.c unipi-values.o
--- template/unipi-values.template.c
+++ template/unipi-values.template.c
@@ -10,99 +10,148 @@
  */
 #include <strings.h>
 #include <stddef.h>
+#include "uniee.h"
 
-struct ptable_struct {
-	int id;
-	const char *name;
+struct slot_dt {
+	int slot;
+	const char *dt;
 };
 
-struct pstable_struct {
+struct id_name {
+	int id;
+	const char *name;
+	const char *dt;
+	const struct slot_dt *slots;
+};
+
+
+struct id_slot_name {
 	int id;
 	int slot;
 	const char *name;
+	const char *dt;
 };
 
-const struct ptable_struct ftable[] = {
-{%- for f,v in platform.family.items() %}
-  { .id={{ "0x%02x" % v }}, .name="{{ f|lower }}" },
-{%- endfor %}
-  {/* sentinel */}
-};
-
-const struct ptable_struct itable[] = {
-{%- for f,v in platform.id.items() %}
-  { .id={{ "0x%04x" % v }}, .name="{{ f|lower }}" },
-{%- endfor %}
-  {/* sentinel */}
-};
-
-const struct ptable_struct btable[] = {
-{%- for f,v in board.model.items() %}
-  { .id={{ "0x%04x" % v }}, .name="{{ f|lower }}" },
-{%- endfor %}
-  {/* sentinel */}
-};
-
-const struct ptable_struct ptable_dt[] = {
-{%- for f,v in product_dt.items() %}
-  { .id={{ f }}, .name="{{ v }}" },
-{%- endfor %}
-  {/* sentinel */}
-};
-
-const struct pstable_struct btable_dt[] = {
+/* board + slot --> overlays */
 {%- for f,x in board_dt.items() %}
+const struct slot_dt c_slot_{{f}}[] = {
   {%- for s,v in x.items() %}
-  { .id={{ f }}, .slot={{s}}, .name="{{ v }}" },
+  { .slot={{s}}, .dt="{{ v }}" },
   {%- endfor %}
+  {/* sentinel */}
+};
 {%- endfor %}
-{/* sentinel */}
+
+/* Family names*/
+const struct id_name family_table[] = {
+{%- for f,v in platform.family.items() %}
+  { .id={{ "0x%02x" % v }}, .name="{{ f }}" },
+{%- endfor %}
+  {/* sentinel */}
 };
 
-static int ptable_by_name(char *name, const struct ptable_struct *table)
+/* product --> name, overlays */
+const struct id_name product_table[] = {
+{%- for f,v in platform.id.items() %}
+  { .id={{ "0x%04x" % v }}, .name="{{ f }}"
+    {%- if v|trim in product_dt -%}
+    , .dt="{{ product_dt[v|trim] }}"
+    {%- endif -%}
+  },
+{%- endfor %}
+  {/* sentinel */}
+};
+
+/* board --> name, overlays, slots */
+const struct id_name board_table[] = {
+{%- for f,v in board.model.items() %}
+  { .id={{ "0x%04x" % v }}, .name="{{ f }}"
+    {%- if v|trim in board_dt -%}
+    , .slots=c_slot_{{v}}
+    {%- endif -%}
+    {#- %- if v|trim in board_dt -%}
+    , .dt="{{ board_dt[v|trim] }}"
+    {%- endif -%  -#}
+  },
+{%- endfor %}
+  {/* sentinel */}
+};
+
+static const struct id_name* get_by_name(char *name, const struct id_name *table)
 {
-	const struct ptable_struct *p;
+	const struct id_name *p;
 	for (p=table; p->name != NULL; p++) {
-		if (strcasecmp(name, p->name) == 0) return p->id;
+		if (strcasecmp(name, p->name) == 0) return p;
 	}
-	return -1;
+	return NULL;
 }
 
-static const char* ptable_by_id(int id, const struct ptable_struct *table)
+static const struct id_name* get_by_id(int id, const struct id_name *table)
 {
-	const struct ptable_struct *p;
+	const struct id_name *p;
 	for (p=table; p->name != NULL; p++) {
-		if (p->id == id) return p->name;
+		if (p->id == id) return p;
+	}
+	return NULL;
+}
+
+static const struct slot_dt* get_by_slot(int slot, const struct slot_dt *table)
+{
+	const struct slot_dt *p;
+	for (p=table; p->dt != NULL; p++) {
+		if (p->slot == slot) return p;
 	}
 	return NULL;
 }
 
 int unipi_family_by_name(char *name)
 {
-	return ptable_by_name(name, ftable);
+	const struct id_name *p = get_by_name(name, family_table);
+	return p ? p->id : -1;
 }
 
-const char* unipi_family_by_id(int id)
+const char* unipi_family_name(platform_id_t id)
 {
-	return ptable_by_id(id, ftable);
+	const struct id_name *p = get_by_id(id.parsed.platform_family, family_table);
+	return p ? p->name : NULL;
 }
 
-int unipi_platform_by_name(char *name)
+int unipi_product_by_name(char *name)
 {
-	return ptable_by_name(name, itable);
+	const struct id_name *p = get_by_name(name, product_table);
+	return p ? p->id : -1;
 }
 
-const char* unipi_platform_by_id(int id)
+const char* unipi_product_name(platform_id_t id)
 {
-	return ptable_by_id(id, itable);
+	const struct id_name *p = get_by_id(id.raw_id, product_table);
+	return p ? p->name : NULL;
 }
 
-int unipi_board_by_name(char *name)
+const char* unipi_product_dt(platform_id_t id)
 {
-	return ptable_by_name(name, btable);
+	const struct id_name *p = get_by_id(id.raw_id, product_table);
+	return p ? p->dt : NULL;
 }
 
-const char* unipi_board_by_id(int id)
+const char* unipi_board_name(board_model_t id)
 {
-	return ptable_by_id(id, btable);
+	const struct id_name *p = get_by_id(id, board_table);
+	return p ? p->name : NULL;
+}
+
+const char* unipi_board_dt(board_model_t id)
+{
+	const struct id_name *p = get_by_id(id, board_table);
+	return p ? p->dt : NULL;
+}
+
+const char* unipi_slot_dt(board_model_t id, int slot)
+{
+	const struct id_name *p = get_by_id(id, board_table);
+	const struct slot_dt *d;
+	if (p == NULL )
+		return NULL;
+	d = get_by_slot(slot, p->slots);
+	return d ? d->dt : NULL;
 }
