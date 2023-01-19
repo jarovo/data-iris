702c1d65d9c52c1cf32723134aec0a33f0310680
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-01-19 14:52:59 +0100

    Add publishing to github.

---
+++ gitlab-ci/mirror-before-script.py
@@ -0,0 +1,2 @@
+cp gitlab-ci/project_specific_scripts/publish-config.sh /ci-scripts
+
---
+++ gitlab-ci/publish-config.sh
@@ -0,0 +1,2 @@
+DST_URL_PREFIX=${DST_URL_PREFIX:-"github.com/UniPiTechnology/"}
+DST_URL_PUBLISH=${DST_URL_PUBLISH:-"$DST_URL_PREFIX/unipi-kernel-modules-published.git"}
