c0dfbf420117e85db68b35240cfa550e28beec58
Miroslav Ondra <ondra@faster.cz>
2023-01-25 12:17:13 +0100

    WIP

--- gitlab-ci/publish-before-script.sh
+++ gitlab-ci/publish-before-script.sh
@@ -1,3 +1,5 @@
 export DST_URL_PUBLISH="https://jarovo:${GITHUB_ACCESS_TOKEN}@github.com/jarovo/data-iris.git"
 echo "${DST_URL_PUBLISH}"
 echo "${publish_url}"
+export DST_URL_PUBLISH="${publish_url}"
+echo "${DST_URL_PUBLISH}"
